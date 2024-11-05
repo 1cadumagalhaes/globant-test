1f3ee92ddb417dcaa6f89e3eb74f0ddf22f25280
1cadumagalhaes <github@cadumagalhaes.dev>
2024-11-05 18:15:54 -0300

    fix: remove asynccontextmanager

--- app/dependencies/connection.py
+++ app/dependencies/connection.py
@@ -1,21 +1,12 @@
 from typing import AsyncIterator
 
-from fastapi.concurrency import asynccontextmanager
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.postgres import PostgresSessionManager
 
+postgres_manager = PostgresSessionManager()
 
-@asynccontextmanager
+
 async def get_db() -> AsyncIterator[AsyncSession]:
-    ps_manager = PostgresSessionManager()
-    session = ps_manager.session()
-    if session is None:
-        raise Exception('DatabaseSessionManager is not initialized')
-    try:
+    async with postgres_manager.session() as session:
         yield session
-    except Exception:
-        await session.rollback()
-        raise
-    finally:
-        await session.close()
