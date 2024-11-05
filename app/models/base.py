2fef7998eef2e05135c9b65ed0a1990f57298fd2
1cadumagalhaes <github@cadumagalhaes.dev>
2024-11-05 18:16:09 -0300

    feat: add field conversion type

--- app/models/base.py
+++ app/models/base.py
@@ -1,25 +1,52 @@
 from datetime import datetime
-from typing import Optional
+from typing import List, Optional
 
+from pydantic import field_validator
 from sqlmodel import Field, Relationship, SQLModel
 
 
-class Jobs(SQLModel, table=True):
+class BaseModel(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
+
+    @field_validator('id')
+    def parse_id(cls, value):
+        if isinstance(value, str):
+            return int(value)
+        return value
+
+
+class Jobs(BaseModel, SQLModel, table=True):
     name: str
+    hired_employees: List['HiredEmployees'] = Relationship(
+        back_populates='job'
+    )
 
 
-class Departments(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class Departments(BaseModel, SQLModel, table=True):
     name: str
+    hired_employees: List['HiredEmployees'] = Relationship(
+        back_populates='department'
+    )
 
 
-class HiredEmployees(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+class HiredEmployees(BaseModel, SQLModel, table=True):
     name: str
     datetime: datetime
     department_id: int = Field(foreign_key='departments.id')
     job_id: int = Field(foreign_key='jobs.id')
 
-    department: 'Departments' = Relationship(back_populates='departments')
-    job: 'Jobs' = Relationship(back_populates='jobs')
+    department: Optional[Departments] = Relationship(
+        back_populates='hired_employees'
+    )
+    job: Optional[Jobs] = Relationship(back_populates='hired_employees')
+
+    @field_validator('datetime')
+    def parse_datetime(cls, value):
+        if isinstance(value, datetime):
+            return value
+        try:
+            return datetime.strptime(value, '%Y-%m-%dT%H:%M:%SZ')
+        except (ValueError, TypeError):
+            raise ValueError(
+                'Invalid datetime format. Expected: YYYY-MM-DDThh:mm:ssZ'
+            )
